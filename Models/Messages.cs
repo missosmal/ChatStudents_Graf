5ff55e71f980bae305c8ef66755353d8ad708619
missosmal <fadesteam8@gmail.com>
2024-03-25 16:20:43 +0500

    Разработанные модели.Graf

---
+++ Models/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Graf.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public Messages(int UserFrom, int UserTo, string Message)
+        {
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Graf.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string  Lastname { get; set; }
+        public string  Firstname { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+    }
+}
