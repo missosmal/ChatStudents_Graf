c27312fc5eb94be834854891433340281e296802
missosmal <fadesteam8@gmail.com>
2024-04-10 16:01:48 +0500

    Разработанные общие классы.Graf

---
+++ Classes/Common/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Graf.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=stedert.permaviat.ru;" +
+            "Trusted_Connection = False;" +
+            "Database = ***;" +
+            "User = ***;" +
+            "Pwd = ***;";
+    }
+}
